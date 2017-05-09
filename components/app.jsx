8df575431de0476de619e76b5f5a69e872e46513
Nishtha <nishtha.arora26@gmail.com>
2017-05-09 00:35:34 -0500

    router with div

--- components/app.jsx
+++ components/app.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Main from './components/Main.jsx';
+import Info from './components/info.jsx';
+import SomethingElse from './components/SomethingElse.jsx';
 
 const App = document.getElementById('app');
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route path="/" component={Main}></Route>
+      <div>
+        <Route path="/" component={Main}></Route>
+        <Route path="/info" component={Info}></Route>
+        <Route path="/somethingElse" component={SomethingElse}></Route>
+      </div>
   </BrowserRouter>,
   App
 );
